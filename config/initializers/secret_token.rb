43e3917a4d692c8f43f29b58d6df37e49327013c
jessicard <jessica@quickleft.com>
2013-04-27 10:44:44 -0600

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymt::Application.config.secret_token = '7569ab217f3f4db7066cab98ae0658bd97b1089979575c22e9988994f21babeb7038efba4dd7c0cfc3130be2ff4946d4ad9bba7cb6d7f286458fab78a85d3a51'
